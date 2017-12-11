7dd16057a933ee04bd6bee3ca087c7522d6a7714
Renato Neto <renatocjn@gmail.com>
2017-12-11 14:35:38 -0300

    fix refile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,4 +2,4 @@
 #Refile.store = Refile::Backend::FileSystem.new(Rails.root.join('public/uploads/refile_store').to_s)
 
 
-#Refile.cdn_host = "http://127.0.0.1:8080"
+Refile.cdn_host = "http://10.30.0.158"
